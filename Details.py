c53b6754f9ea8a88c6961d56864074b92aee9bad
Mohammed Sami Al-Ghoraibi <141200991+MOSAMI0@users.noreply.github.com>
2024-01-19 20:58:40 +0300

    Delete Details.py

--- Details.py
+++
@@ -1,4 +0,0 @@
-name = input('Enter your name please: ').upper()
-age = int(input('Enter your age: '))
-major = input('What is your major ? ').upper()
-print("Welcome :)", name , ".Your age is great:",age, ",and your major is fantastic: ",major)
