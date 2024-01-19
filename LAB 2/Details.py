2def0b5fd201eec16fe294a1a506724034957a44
Mohammed Sami Al-Ghoraibi <141200991+MOSAMI0@users.noreply.github.com>
2024-01-19 21:12:35 +0300

    Add files via upload

---
+++ LAB 2/Details.py
@@ -0,0 +1,4 @@
+name = input('Enter your name please: ').upper()
+age = int(input('Enter your age: '))
+major = input('What is your major ? ').upper()
+print("Welcome :)", name , ".Your age is great:",age, ",and your major is fantastic: ",major)
